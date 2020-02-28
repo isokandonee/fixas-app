5c4c6385ba2b6db993fc01771b0498f98f96c7d1
Ogunniyi Isokan Mitchel <isokandone@gmail.com>
2020-02-28 15:40:54 +0100

    Signup form and dashboard created

---
+++ dashboard/index.php
@@ -0,0 +1,17 @@
+<?php
+    include "../include/header.php";
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-6 bg-light">
+            
+        </div>
+        <div class="col-sm-6 bg-light">
+            
+        </div>
+    </div>
+</div>
+<?php
+    include "../include/footer.php";
+?>
