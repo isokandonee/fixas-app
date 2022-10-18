833a421c3e6386e35f84ef49d4d1e052bcf00f6f
Ogunniyi Isokan <isokandone@gmail.com>
2022-10-18 22:47:01 +0100

    added live database
    Added a live online database

--- controller/connect.php
+++ controller/connect.php
@@ -1,5 +1,10 @@
 <?php
-$conn = mysqli_connect('localhost', 'root', '', 'users');
+$db_host = "sql8.freemysqlhosting.net";
+$db_user = "sql8527596";
+$db_pass = "9qWHei1rDr";
+$db_name = "sql8527596";
+
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
 // Check if connection to db is successful
 if (!$conn){
